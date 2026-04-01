1b8cfa70a33df124d469fa72504acf3740853d85
vapostolakos <114405589+vapostolakos@users.noreply.github.com>
2026-04-01 21:07:17 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBzrEe59ikGEGaYoF6hmSPX6bwQTXLXgDk",
+  authDomain: "quiz-nights-f1e26.firebaseapp.com",
+  projectId: "quiz-nights-f1e26",
+  storageBucket: "quiz-nights-f1e26.firebasestorage.app",
+  messagingSenderId: "804082374858",
+  appId: "1:804082374858:web:075c9e92b9a26b65597c33"
+};
+
+window.QUIZ_ADMIN = {
+  username: "vassilis",
+  password: "04091986"
+};
